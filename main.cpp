1cb725a44385822407dc1dcd5ef0334fc124dcf6
YouCantTriforce <michal.wilski@gmail.com>
2015-10-05 17:29:46 +0200

    making sure that machine running _1byte has correct endianness (LE); test startup of CPU and Memory modules

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "cpu/CPU.h"
+#include "memory/memory.h"
+#include <cassert>
+
+static bool check_le()
+{
+    const uint8_t array[4] = { 44, 1, 0, 0 };
+
+    assert(sizeof(array) == sizeof(uint32_t));
+    return reinterpret_cast<const uint32_t&>(array[0]) == 300;
+}
 
 int main(int argc, char** argv)
 {
-    //future entry point
+    if(!check_le())
+    {
+        std::cout << "Unsupported architecture (big-endian)" << std::endl;
+        return 1;
+    }
+
+    cpu::CPU::instance();
+    memory::Memory::instance();
+
+
 
     return 0;
 }
