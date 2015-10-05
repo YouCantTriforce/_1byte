affa99ca406bc17bdf0763fbe38b2f5d1298ce01
YouCantTriforce <michal.wilski@gmail.com>
2015-10-05 17:28:23 +0200

    Memory is being a singleton now; draft for read/write operations

--- memory/memory.cpp
+++ memory/memory.cpp
@@ -1,12 +1,25 @@
 #include "memory.h"
 
+#include <iostream>
+
 namespace memory
 {
     Memory::Memory()
     {
+        std::cout << "memory online" << std::endl;
     }
 
     Memory::~Memory()
     {
+    }
+
+    char Memory::readByte(char& address, const char& byte)
+    {
+        //TODO implement
+    }
+
+    void Memory::writeByte(char& address, const char& byte)
+    {
+        //TODO implement
     }
 }
--- memory/memory.h
+++ memory/memory.h
@@ -1,13 +1,21 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include "../utils/singleton.h"
+#include "memory_locations.h"
+
 namespace memory
 {
-    class Memory
+    class Memory:
+        public Singleton<Memory>
     {
     public:
         Memory();
         ~Memory();
+
+    public:
+        char readByte(char& address, const char& byte);
+        void writeByte(char& address, const char& byte);
 
     private:
         char m_storage[8192];
